ef62ed4cf41f0c8dbac913af72936d9e76539751
nanoharon <nanoxaronweriop@gmail.com>
2026-07-27 21:00:24 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет!")
+
+bot.infinity_polling()
